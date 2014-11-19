138706f47080193c06beea4d11c4f69d5c4d2d08
Adrian <acglez10@gmail.com>
2014-11-19 17:56:26 +0000

    Cumplida expectativa creada para la clase Interface, donde el usuario realiza el examen.

---
+++ lib/exam/interface.rb
@@ -0,0 +1,36 @@
+require_relative 'examen'
+
+class Interface
+	#attr_reader :exam
+	
+	def initialize(exam)
+		@theExam = exam
+		@userAnswers = Array.new(@theExam.numQuestion)
+		@numRight = 0
+	end
+
+	def make_exam
+		i = 0
+		puts @theExam.printQuestion
+		while @userAnswers.size() > i
+			puts @theExam.printQuestion
+			print "--> Respuesta = "
+			@userAnswers[i] = gets.chomp
+			if @userAnswers[i] == @theExam.examAnswers[i]
+				@numRight += 1
+			end
+			i += 1
+		end
+		puts " \n---- Resultado del examen ------ "
+		if pass
+			puts " ## --> Aprobado."
+		else
+			puts " ## --> Suspendido."
+		end
+	end
+
+	private
+	def pass
+		@userAnswers.size()/2 < @numRight
+	end
+end
