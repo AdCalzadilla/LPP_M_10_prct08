48ffd45b7fe656df835d380238e23ac83d1e6d0c
Adrian <acglez10@gmail.com>
2014-11-19 17:40:37 +0000

    Creada la clase Examen con éxito.

---
+++ lib/exam/examen.rb
@@ -0,0 +1,26 @@
+require_relative 'Simple_Selection'
+require_relative 'list'
+require_relative 'trueFalse'
+
+class Examen
+
+	attr_reader :examAnswers
+
+	def initialize(questions,answers)
+		@myExam = List.new("\n\tLenguajes y Paradigmas de la programación.\n\t\tExamen práctico.\n\n")
+		@myExam.add_many(questions)
+		@examAnswers = answers
+	end
+
+	def to_s
+		@myExam.to_s
+	end
+
+	def printQuestion
+		@myExam.pop()
+	end
+
+	def numQuestion
+		@examAnswers.length
+	end
+end
